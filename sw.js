74d57c8ddc867f171a74104ff06b7f18fd1c8f30
watanabehidetaka <59433677+watanabehidetaka@users.noreply.github.com>
2022-06-23 06:54:29 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'uuidgen-v0.0.4';
+const CACHE_NAME = 'uuidgen-v0.1.0';
 const OFFLINE_URL = '/uuidgen/';
 const urlsToCache = [
   '/uuidgen/',
@@ -10,19 +10,21 @@ const urlsToCache = [
 ];
 const neverCacheUrls = /\/wp-admin|\/wp-login|preview=true|\/cart|ajax|login/;
 
-self.addEventListener('install', (event) => {
-	event.waitUntil(
-		caches.open(CACHE_NAME).then((cache) => {
-			return cache.addAll(urlsToCache);
-		})
-	);
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[Service Worker] Caching all: app shell and content');
+      return cache.addAll(contentToCache);
+    })
+  );
 });
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keyList) => {
-          return Promise.all(keyList.map((key) => {
-        if(key !== cacheName) {
+      return Promise.all(keyList.map((key) => {
+        if(key !== CACHE_NAME) {
           return caches.delete(key);
         }
       }));
@@ -31,62 +33,16 @@ self.addEventListener('activate', (e) => {
 });
 
 self.addEventListener('fetch', (e) => {
-	if (!e.request.url.match(/^(http|https):\/\//i)) {
-		return;
-	}
-
-	if (new URL(e.request.url).origin !== location.origin) {
-		return;
-	}
-
-	if (neverCacheUrls.test(e.request.url)) {
-		return;
-	}
-	if (neverCacheUrls.test(e.request.referrer)) {
-		return;
-	}
-
-	if (e.request.referrer.match(/^(wp-admin):\/\//i)) {
-		return;
-	}
-
-	if (e.request.method !== 'GET') {
-		e.respondWith(
-			fetch(e.request).catch(() => {
-				return caches.match(OFFLINE_URL);
-			})
-		);
-		return;
-	}
-
-	if (e.request.mode === 'navigate' && navigator.onLine) {
-		e.respondWith(
-			fetch(e.request).then((response) => {
-				return caches.open(CACHE_NAME).then((cache) => {
-					cache.put(e.request, response.clone());
-					return response;
-				});
-			})
-		);
-		return;
-	}
-
-	e.respondWith(
-		caches
-			.match(e.request)
-			.then((response) => {
-				return (
-					response ||
-					fetch(e.request).then((response) => {
-						return caches.open(CACHE_NAME).then((cache) => {
-							cache.put(e.request, response.clone());
-							return response;
-						});
-					})
-				);
-			})
-			.catch(() => {
-				return caches.match(OFFLINE_URL);
-			})
-	);
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+      console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then((response) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
 });
