6524a9dcbc1bf34ed70069442ed02188c80e33d2
watanabehidetaka <59433677+watanabehidetaka@users.noreply.github.com>
2022-06-23 07:46:34 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'uuidgen-v0.1.0';
+const cacheName = 'uuidgen-v0.1.1';
 const OFFLINE_URL = '/uuidgen/';
 const urlsToCache = [
   '/uuidgen/',
@@ -13,7 +13,7 @@ const neverCacheUrls = /\/wp-admin|\/wp-login|preview=true|\/cart|ajax|login/;
 self.addEventListener('install', (e) => {
   console.log('[Service Worker] Install');
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
+    caches.open(cacheName).then((cache) => {
       console.log('[Service Worker] Caching all: app shell and content');
       return cache.addAll(contentToCache);
     })
@@ -24,7 +24,7 @@ self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(keyList.map((key) => {
-        if(key !== CACHE_NAME) {
+        if(key !== cacheName) {
           return caches.delete(key);
         }
       }));
@@ -37,7 +37,7 @@ self.addEventListener('fetch', (e) => {
     caches.match(e.request).then((r) => {
       console.log('[Service Worker] Fetching resource: '+e.request.url);
       return r || fetch(e.request).then((response) => {
-        return caches.open(CACHE_NAME).then((cache) => {
+        return caches.open(cacheName).then((cache) => {
           console.log('[Service Worker] Caching new resource: '+e.request.url);
           cache.put(e.request, response.clone());
           return response;
