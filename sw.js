10a7e07d5fb040ef6321d47eb784b9091313935b
watanabehidetaka <59433677+watanabehidetaka@users.noreply.github.com>
2022-06-25 02:53:11 +0000

    Update sw.js, version change

--- sw.js
+++ sw.js
@@ -1,27 +1,26 @@
-const cacheName = 'uuidgen-v0.1.6';
+const cacheName = 'uuidgen-v0.1.7';
 const urlsToCache = [
   '/uuidgen/',
-  '/uuidgen/index.html',
   '/uuidgen/app.js',
   '/uuidgen/uuidgen.js',
   '/uuidgen/style.css',
   '/uuidgen/icon-64.png'
 ];
 
-self.addEventListener('install', (e) => {
+self.addEventListener('install', e => {
   console.log('[Service Worker] Install');
   e.waitUntil(
-    caches.open(cacheName).then((cache) => {
+    caches.open(cacheName).then(cache => {
       console.log('[Service Worker] Caching all: app shell and content');
       return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener('activate', (e) => {
+self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
         if(key !== cacheName) {
           return caches.delete(key);
         }
@@ -30,12 +29,12 @@ self.addEventListener('activate', (e) => {
   );
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then((r) => {
+    caches.match(e.request).then(r => {
       console.log('[Service Worker] Fetching resource: '+e.request.url);
-      return r || fetch(e.request).then((response) => {
-        return caches.open(cacheName).then((cache) => {
+      return r || fetch(e.request).then(response => {
+        return caches.open(cacheName).then(cache => {
           console.log('[Service Worker] Caching new resource: '+e.request.url);
           cache.put(e.request, response.clone());
           return response;
